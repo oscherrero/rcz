53f44ba68decbef8068534fe7f4c39997807ec0f
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-03-10 07:21:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,24 +10,8 @@ const temas=[
 
 window.onload=init()
     
-function cargarListaGranjas(options){
-  document.getElementById("granja").innerHTML = options
-  var stGranjaCod = localStorage.getItem("granja") != null ? localStorage.getItem("granja") : '{"granja":"","cod":""}';
-  var granjaCod = JSON.parse(stGranjaCod)
-  document.getElementById("granja").value = granjaCod.granja
-  document.getElementById("cod").value = granjaCod.cod
-}
-   
 function init() {
-  var options =localStorage.getItem("options")
-  
-  cargarListaGranjas(options)
-
-  var tema=localStorage.getItem("tema")!=null?parseInt(localStorage.getItem("tema"))-1:10;
-  localStorage.setItem("tema",tema)
   cambiarTema()
-  cargarHTML()
-
 }
 
 function recargarWeb (){  location.href = location.href + "?" + new Date().getTime();}
@@ -36,65 +20,6 @@ function info() {
   const anchoPantalla = window.innerWidth;
   const resPantalla = window.devicePixelRatio
   document.getElementById("nota").textContent = " width: " + anchoPantalla + ", res: " + resPantalla;
-}
-
-async function cargarHTML() {
-
-  document.getElementById("elemento1").classList.remove("oculto");
-  document.getElementById("logForm").classList.add("oculto");
-  document.getElementById("elemento1").innerHTML = "<div class='imagenEspera'><img  src='" + imgEsperaUrl + "'></div> <br><br>"
- 
-  try {
-    
-    const formData= new FormData(document.getElementById("logForm"))
-    const granjaInput= document.getElementById("granja").value
-    const codInput= document.getElementById("cod").value
-
-   url = webUrl + "?file=bloque";
-    const response = await fetch(url, {
-        method: 'POST',
-        body: formData,
-    })
-
-  var resp = await response.json()
-  cargarListaGranjas(resp.optionsHtml)
-
-  if (resp.html == "NoAuth" ) {
-      document.getElementById("elemento1").innerHTML = "<div style='color:red; text-align:center;'><br> INDICA UNA GRANJA Y CLAVE VALIDOS <br></div>"
-      document.getElementById("tituloGranja").innerHTML = "PREMIER PIGS";
-      document.getElementById("cod").value =""
-      document.getElementById("logForm").classList.remove("oculto");
-    } else   if ( resp.html == "") {
-      document.getElementById("tituloGranja").innerHTML = "PREMIER PIGS";
-      document.getElementById("cod").value =""
-      verLogForm()
-    } else {     
-      document.getElementById("elemento1").innerHTML = resp.html
-      document.getElementById("tituloGranja").innerHTML =  granjaInput.toUpperCase() 
-      document.getElementById("logForm").classList.add("oculto");
-    
-      var valor = JSON.stringify({ granja: granjaInput, cod: codInput })
-      localStorage.setItem('granja', valor);
-      localStorage.setItem("options",resp.optionsHtml)
-    }
-  } catch (error) {
-    document.getElementById("elemento1").innerHTML = "<div style='color:red; text-align:center;'><br> FALLO EN LA CONSULTA DE DATOS </div>"
-    document.getElementById("elemento1").innerHTML = error
-    document.getElementById("tituloGranja").innerHTML = "PREMIER PIGS";
-
-  }  
-}
-
-function recuperarListaGranjas(){
-  var stGranjaCod = localStorage.getItem("granja") != null ? localStorage.getItem("granja") : '{"granja":"","cod":""}';
-  var granjaCod = JSON.parse(stGranjaCod)
-  document.getElementById("granja").value = granjaCod.granja
-  document.getElementById("cod").value = granjaCod.cod
-}
-
-function verLogForm(){
-  document.getElementById("logForm").classList.remove("oculto");
-  document.getElementById("elemento1").classList.add("oculto");
 }
 
 async function chiste(){ 
@@ -114,7 +39,7 @@ async function chiste(){
 
 function acercade(){ 
   const elem1 = document.getElementById("elemento1") 
-  elem1.innerHTML = "<div class='marco'><h3>Demo conexión datos AVEVA INSIGHT</h3><p>Realizada por OscarHR con Google Apps Script.</p><p>04/01/2025</p></div> <br><br>"
+  elem1.innerHTML = "<div class='marco'><h3>ENCABEZADO ACERCA DE</h3><p>Realizada por OscarHR.</p><p>04/01/2025</p></div> <br><br>"
   elem1.innerHTML += "<div> Ancho: "+ window.innerWidth +"</div>"
   elem1.innerHTML += "<div> Alto: "+ window.innerHeight; +"</div>"
   elem1.innerHTML += "<div> Resolucion: "+ window.devicePixelRatio  +"</div>"
